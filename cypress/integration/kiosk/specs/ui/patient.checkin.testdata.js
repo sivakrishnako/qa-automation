90ece663d874d367f44bc3b82406cb03966931b0
ylnagaraj <nagarajuy@cybage.com>
2022-05-19 16:44:09 +0530

    First commit

---
+++ cypress/integration/kiosk/specs/ui/patient.checkin.testdata.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+class patientData {
+
+    static validLastName   = "Alma"
+    static validDOB        = "01/01/1990"
+    static invalidLastName = "warner"
+    static invalidDOB      = "01/01/1990"
+
+    static popupMsg    = "Please check in at the front desk."
+
+} 
+export default patientData;
